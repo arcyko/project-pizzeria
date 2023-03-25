26a5c8a488c7700e1d7c1fdf1eff8deb0a562869
Arkadiusz Piątek <piatek.arkadiusz@gmail.com>
2023-03-25 09:39:52 +0100

    Add accordion

--- dist/js/script.js
+++ dist/js/script.js
@@ -82,47 +82,38 @@
 
     initAccordion(){
       const thisProduct = this;
-  
+    
       /* find the clickable trigger (the element that should react to clicking) */
       const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
-  
+    
       /* START: add event listener to clickable trigger on event click */
       clickableTrigger.addEventListener('click', function(event) {
         
         /* prevent default action for event */
         event.preventDefault();
-  
+    
         /* find active product (product that has active class) */
         const activeProduct = document.querySelector(select.menuProduct.active);
-  
+    
         /* if there is active product and it's not thisProduct.element, remove class active from it */
         if (activeProduct && activeProduct !== thisProduct.element) {
           activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
         }
-  
+    
         /* toggle active class on thisProduct.element */
         thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
-      });
-
-      /* add event listener to each product option */
-      const productOptions = thisProduct.element.querySelectorAll(select.menuProduct.option);
-      for (let productOption of productOptions) {
-        productOption.addEventListener('click', function() {
-
-          /* find active option for this product */
-          const activeOption = thisProduct.element.querySelector(select.menuProduct.activeOption);
-
-          /* if there is active option and it's not this option, remove class active from it */
-          if (activeOption && activeOption !== productOption) {
-            activeOption.classList.remove(classNames.menuProduct.wrapperActive);
+    
+        /* find all products */
+        const allProducts = document.querySelectorAll(select.all.menuProducts);
+    
+        /* iterate over each product */
+        for (let activeProduct of allProducts) {
+          /* if product is not the same as thisProduct, remove active class */
+          if (activeProduct !== thisProduct.element) {
+            activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
           }
-
-          /* toggle active class on this option */
-          productOption.classList.toggle(classNames.menuProduct.wrapperActive);
+        }
       });
-      
-    }
-  
     }
   
   }
