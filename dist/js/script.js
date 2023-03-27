0b436a37cc9933886a5445af4774842a7ebdecec
Arkadiusz Piątek <piatek.arkadiusz@gmail.com>
2023-03-27 11:55:49 +0200

    Add images to processOrder

--- dist/js/script.js
+++ dist/js/script.js
@@ -39,7 +39,7 @@
       imageVisible: 'active',
     },
   };
-  
+
   /*
   const settings = {
     amountWidget: {
@@ -93,6 +93,7 @@
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
     }
 
     initAccordion(){
@@ -169,19 +170,32 @@
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
           console.log(optionId, option);
+    
+          // find image for pair paramId-optionId
+          const optionImage = thisProduct.imageWrapper.querySelector(`.${paramId}-${optionId}`);
 
           // check if there is param with a name of paramId in formData and if it includes optionId
-          if(formData[paramId] && formData[paramId].includes(optionId)) {
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
 
+          if(optionImage) {
+            if(optionSelected) {
+              optionImage.classList.add('active');
+            } else {
+              optionImage.classList.remove('active');
+            }
+          }
+    
+          if(optionSelected) {
+    
             // check if the option is not default
             if(!option.default){
-
+    
               // add option price to price variable
               price += option.price;
             }
           } else {
           // check if the option is default
-
+    
             if(option.default) {
             // reduce price variable
               price -= option.price;
